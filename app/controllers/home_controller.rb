92e5bb4e6527957f498ddbe99cc3f7d9aa989355
dongwonkay <dongwon@likelion.org>
2017-05-27 10:49:45 +0000

    로그인됫다 ㅠㅠ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @yolo_play = ["travel", "sport", "study"].sample
+    
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
   end
 end
