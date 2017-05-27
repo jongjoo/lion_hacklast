700bba16467d54b7201365b5f8d4984a62b62395
jongjoo <jjongju8790@gmail.com>
2017-05-27 10:55:26 +0000

    facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  def login
+    @f_user = User.koala(request.env['omniauth.auth']['credentials'])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  
+  get 'home/login'
+  get 'auth/facebook', as: "auth_provider"
+  get 'auth/facebook/callback', to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
